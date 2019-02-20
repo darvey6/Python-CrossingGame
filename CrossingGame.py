efd54566df65bd04fb2b1caacda12b0fce123885
Darvey <darvey6@hotmail.com>
2019-02-19 21:37:39 -0800

    implemented collision detection

--- CrossingGame.py
+++ CrossingGame.py
@@ -9,6 +9,7 @@ WHITE_COLOR = (255, 255, 255)
 BLACK_COLOR = (0, 0, 0)
 
 
+
 class Game:
     
     TICK_RATE = 60
@@ -27,6 +28,8 @@ class Game:
         direction = 0
         player_character = PlayerCharacter("player.png", 375, 700, 50, 50)
         enemy_0 = EnemyCharacter('enemy.png', 20, 400, 50, 50)
+        enemy_1 = EnemyCharacter('enemy.png', 600, 600, 50, 50)
+        treasure = GameObject('treasure.png', 375, 75 , 50, 50)
 
         # Game Loop
         while not is_game_over:
@@ -49,10 +52,22 @@ class Game:
                 print(event)
 
             self.game_screen.fill(WHITE_COLOR)
-            player_character.move(direction)
+
+            treasure.draw(self.game_screen)
+            
+            player_character.move(direction, self.height)
             player_character.draw(self.game_screen)
+            
             enemy_0.move(self.width)
             enemy_0.draw(self.game_screen)
+
+            if player_character.detect_collision(enemy_0):
+                is_game_over = True
+            elif player_character.detect_collision(treasure):
+                is_game_over= True
+                
+            enemy_1.move(self.width)
+            enemy_1.draw(self.game_screen)
             
        
             # Update all graphics         
@@ -90,8 +105,21 @@ class PlayerCharacter(GameObject):
         elif direction < 0:
             self.y_pos += self.SPEED
 
-        if self.y_pos >= max_height - 20:
-            self.y_pos = nax_hegiht - 20
+        if self.y_pos >= max_height - 50:
+            self.y_pos = max_height - 50
+
+    def detect_collision(self, other_body):
+        if self.y_pos > other_body.y_pos + other_body.height:
+            return False
+        elif self.y_pos + self.height < other_body.y_pos:
+            return False
+
+        if self.x_pos > other_body.x_pos + other_body.width:
+            return False
+        elif self.x_pos + self.width < other_body.x_pos:
+            return False
+        
+        return True
 
 class EnemyCharacter(GameObject):
 
