abcbc6cd4d51a901a672f36cda8f4d1d35c6f937
Darvey <darvey6@hotmail.com>
2019-02-19 20:41:01 -0800

    made player object

--- CrossingGame.py
+++ CrossingGame.py
@@ -13,7 +13,6 @@ class Game:
     
     TICK_RATE = 60
 
-
     def __init__(self, title, width, height):
         self.title = title
         self.width = width
@@ -25,25 +24,80 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
+        player_character = PlayerCharacter("player.png", 375, 700, 50, 50)
 
         # Game Loop
         while not is_game_over:
-
             # Gets all event occuring at a time
             for event in pygame.event.get():
                 # if quit type event then exit loop
                 if event.type == pygame.QUIT:
                     is_game_over = True
+                    
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                        
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
 
                 print(event)
+
+            self.game_screen.fill(WHITE_COLOR)
+            player_character.move(direction)
+            player_character.draw(self.game_screen)
        
             # Update all graphics         
             pygame.display.update()
             clock.tick(self.TICK_RATE)
 
 
-       
+class GameObject:
+    
+    def __init__(self, image_path, x, y, width, height):
+        object_image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(object_image, (width, height))
 
+        self.x_pos = x
+        self.y_pos = y
+
+        self.width = width
+        self.height = height
+        
+    def draw(self, background):
+        background.blit(self.image, (self.x_pos, self.y_pos))
+
+
+
+class PlayerCharacter(GameObject):
+
+    SPEED = 10
+    
+    def __init__ (self,image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
+        
+
+class EnemyCharacter(GameObject):
+
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, direction):
+        
+
+        
 pygame.init()
 
 new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
